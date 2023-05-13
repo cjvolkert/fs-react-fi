c697c2e7ae6deecf79c9904c7436ee81c20d5c8c
cjv <christianjvolkert@gmail.com>
2023-05-13 20:15:05 +0200

    Exercises 3.9.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -45,6 +45,7 @@ app.use(
     }
   })
 );
+app.use(express.static("build"));
 
 app.get("/api/persons", (req, res) => {
   res.json(data);
@@ -108,7 +109,7 @@ app.post("/api/persons/", (req, res) => {
   res.json(entry);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
